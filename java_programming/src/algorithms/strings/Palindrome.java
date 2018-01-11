86ec70ddec6caefefabfa7240c638d3cfe09564d
kj20207 <kj20207@iastate.edu>
2018-01-11 16:18:33 -0600

    Push code for Palindrome

--- java_programming/src/algorithms/strings/Palindrome.java
+++ java_programming/src/algorithms/strings/Palindrome.java
@@ -2,5 +2,8 @@ package algorithms.strings;
 
 public class Palindrome {
 
+	public static boolean palindrome(String s){
+		return s.equals(ReverseString.reverse(s));
+	}
 	
 }
--- java_programming/src/testing/algorithms/StringsTest.java
+++ java_programming/src/testing/algorithms/StringsTest.java
@@ -15,13 +15,18 @@ public class StringsTest {
 		String original = g.randomStringAlpha(15);
 		
 		System.out.println("--- String: " + original);
-		System.out.println("- Reversed: " + ReverseString.reverse(original));
+		System.out.println("--- Reversed: " + ReverseString.reverse(original));
 		
 		String anagram1 = "listen";
 		String anagram2 = "silent";
 		String anagram3 = "wowowo";
 		System.out.println("-- Anagram: " + Anagram.anagram(algorithm, anagram1, anagram2));
 		System.out.println("-- Anagram: " + Anagram.anagram(algorithm, anagram2, anagram3));
+		
+		String palindrome1 = "amanaplanacanalpanama";
+		String palindrome2 = "palindrome";
+		System.out.println("-- Palindrome: " + Palindrome.palindrome(palindrome1));
+		System.out.println("-- Palindrome: " + Palindrome.palindrome(palindrome2));
 	}
 	
 }
